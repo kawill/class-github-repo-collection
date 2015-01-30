9bf0bb6536945157ea9de81a5f5335b13f7e5b46
Kristen <kawilliams3@gmail.com>
2015-01-30 15:41:46 -0600

    tryagain

--- js/github.js
+++ js/github.js
@@ -23,16 +23,20 @@
     function GithubClient(token) {
         this.token = token;
         this.members = [];
-        this.repos = [];
+        // this.repos = [];
         var self = this;
 
         var GithubRouter = Backbone.Router.extend({
             routes: {
                 ":username": "drawUserInfo"
+                    // "/#username/:repo": "drawRepoInfo",
             },
             drawUserInfo: function(username) {
                 self.drawUser(username)
             },
+            // drawRepoInfo: function(repo) {
+            //     self.drawRepo2(repo);
+            // },
             initialize: function() {
                 Backbone.history.start();
             }
@@ -44,8 +48,8 @@
 
     GithubClient.prototype = {
         URLs: {
-            members: "https://api.github.com/orgs/TIY-Houston-Front-End-Engineering/members",
-            repos: "http://api.github.com/users/" + this.members + "/repos?access_token=" + this.token
+            members: "https://api.github.com/orgs/TIY-Houston-Front-End-Engineering/members"
+            // repos: "http://api.github.com/users/" + this.username + "/repos"
         },
         access_token: function() {
             return "?access_token=" + this.token
@@ -86,56 +90,90 @@
                 var left_column = document.querySelector(".github-grid > *:nth-child(1)");
                 left_column.innerHTML = _.template(html, {
                     members: members
-                });
+                }); //rearrange data feed for _.template("string", {object})
             })
         },
 
         drawUser: function(username) {
-            alert(username)
-        }
+            getUserInfo = function() {  
+                return $.get("http://api.github.com/users/" + this.username + "?access_token=" + this.token)
+            };
+
+            getReposInfo = function() {  
+                return $.get("http://api.github.com/users/" + this.username + "/repos?access_token=" + this.token)
+            };
+
+            getTemplateLeft = function() {  
+                return $.get("./templates/userInfo.html")
+            };
+
+            getTemplateRightRepo = function() {  
+                return $.get("./templates/repos.html")
+            };
+
+            getAllData = function() {  
+                return $.when(this.getUserInfo(), this.getReposInfo(), this.getTemplateLeft(), this.getTemplateRightRepo())
+            };
+
+            drawToPage = function() {  
+                this.getAllData().then(function(userInfo, repoInfo, templateLeft, templateRightRepo) {
+
+                    document.body.querySelector('.userInfo').innerHTML = _.template(templateLeft[0], userInfo[0]);
+
+                    var current = {};
+
+                    for (var i = 0; i < repoInfo[0].length; i++) {
+                        current = repoInfo[0][i];
+
+                        document.body.querySelector('.repo').innerHTML += _.template(templateRightRepo[0], current);
+
+                    }
+
+                })
+            };
+
+        },
 
         // /////////////
 
 
 
-            getRepoData: function() {
-            var x = $.Deferred();
+        // getRepoData: function() {
+        //     var x = $.Deferred();
 
-            if (this.repos.length > 0) {
-                x.resolve(this.repos);
-            } else {
-                var p = $.get(this.URLs.repos + this.access_token());
-                p.then(function(repoData) {
-                    x.resolve(repoData);
-                    this.repos = repoData;
-                })
-            }
+        //     if (this.repos.length > 0) {
+        //         x.resolve(this.repos);
+        //     } else {
+        //         var p = $.get(this.URLs.repos + this.access_token());
+        //         p.then(function(repoData) {
+        //             x.resolve(repoData);
+        //             this.repos = repoData;
+        //         })
+        //     }
 
-            return x;
-        },
+        //     return x;
+        // },
 
-        loadTemplate: function(repos) {
-            // modify the event context, return only the data
-            return $.get("./templates/" + repos + ".html").then(function(repoData) {
-                return repoData;
-            })
-        },
+        // loadTemplateRepo: function(repos) {
+        //     // modify the event context, return only the data
+        //     return $.get("./templates/" + repos + ".html").then(function(repoData) {
+        //         return repoData;
+        //     })
+        // },
 
-        draw: function() {
-            $.when(
-                this.getRepoData(),
-                this.loadTemplate("repos")
-            ).then(function(repos, html) {
-                var right_column = document.querySelector(".github-grid > *:nth-child(1)");
-                right_column.innerHTML = _.template(html, {
-                    repos: repos
-                });
-            })
-        },
+        // drawRepo: function() {
+        //     $.when(
+        //         this.getRepoData(),
+        //         this.loadTemplateRepo("repos")
+        //     ).then(function(repos, html) {
+        //         var right_column = document.querySelector(".github-grid > *:nth-child(1)");
+        //         right_column.innerHTML = _.template(html, {
+        //             repos: repos
+        //         });
+        //     })
+        // },
 
-        // drawUser: function(username) {
-        //     alert(username)
-        // }
+
 
 
     }
@@ -143,5 +181,3 @@
     window.GithubClient = GithubClient;
 
 })();
-
-
